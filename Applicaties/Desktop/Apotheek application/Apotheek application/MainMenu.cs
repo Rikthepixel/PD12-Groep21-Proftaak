e8df3e512dab39a9634e455514cd2f82b49e869c
RIk <Rikdenbreejen@hotmail.nl>
2020-11-04 16:10:15 +0100

    [Change] Set minimum size
    Set minimum size of main menu

--- Applicaties/Desktop/Apotheek application/Apotheek application/MainMenu.cs
+++ Applicaties/Desktop/Apotheek application/Apotheek application/MainMenu.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace Apotheek_application
 {
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
         }
+
+
     }
 }
