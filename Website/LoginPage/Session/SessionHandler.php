e6577c9d77311ec3487dbf56ef37bf67460815a2
RIk <Rikdenbreejen@hotmail.nl>
2020-11-10 13:42:04 +0100

    [Addition] Basic Session Functions

--- Website/LoginPage/Session/SessionHandler.php
+++ Website/LoginPage/Session/SessionHandler.php
@@ -1,3 +1,14 @@
+<!--Rik werkt hier-->
 <?php
+session_start(); // Starts a session
 
+//Gives the ability to carry over variables to other pages with a session
+function StoreSessionVariable($VariableName, $VariableValue){
+    $_SESSION["$VariableName"] = $VariableValue;
+}
+
+//Gives the ability to call perviously stored variables within a session
+function ReadSessionVariable($VariableName){
+    return $_SESSION["$VariableName"];
+}
 ?>
