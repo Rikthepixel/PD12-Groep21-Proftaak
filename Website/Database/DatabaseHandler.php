8dca1352a3d07e527c386ccb5141b94558c43893
RikThePixel <Rikdenbreejen@hotmail.nl>
2020-11-11 09:47:21 +0100

    [Fix] DatabaseHandler fix syntax error

--- Website/Database/DatabaseHandler.php
+++ Website/Database/DatabaseHandler.php
@@ -4,15 +4,17 @@
     $logindbusername = "Login";
     $logindbpassword = "LoginUser";
 
-    $Loginconn = mysqli_connect($dbservername, $logindbusername, $logindbpassword, $dbname);
-
+    $Loginconn = mysqli_connect($dbservername, $logindbusername, $logindbpassword, $logindbname);
+    if (!$Loginconn){
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
     //$dbservername = "192.168.44.186"; <-- de server
     //$dbname = "Users"; <- De database die je wilt zien
     //$dbusername = "Login"; <-- De database user zijn username
     //$dbpassword = "LoginUser"; <-- de database user zijn password
 
     //$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
-
-    if (!$conn){
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    //if (!$conn){
+       // die("Connection failed: " . mysqli_connect_error());
+    }//
