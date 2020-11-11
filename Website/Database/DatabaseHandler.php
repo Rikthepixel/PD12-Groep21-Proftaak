6cff1707e7b68170603de1a6e95e0112a32069d1
RikThePixel <Rikdenbreejen@hotmail.nl>
2020-11-11 10:12:44 +0100

    [Fix] commented out a stray

--- Website/Database/DatabaseHandler.php
+++ Website/Database/DatabaseHandler.php
@@ -8,7 +8,7 @@
     if (!$Loginconn){
         die("Connection failed: " . mysqli_connect_error());
     }
-    
+
     //$dbservername = "192.168.44.186"; <-- de server
     //$dbname = "Users"; <- De database die je wilt zien
     //$dbusername = "Login"; <-- De database user zijn username
@@ -17,4 +17,4 @@
     //$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
     //if (!$conn){
        // die("Connection failed: " . mysqli_connect_error());
-    }//
+    //}
