77027917bb37d8613c2403b950e78bfa9963c3af
RIk <Rikdenbreejen@hotmail.nl>
2020-11-30 15:25:31 +0100

    Auto stash before merge of "Application" and "origin/Application"

--- Applicaties/Desktop/Appotheek application/Apotheek application/MasterPage.Designer.cs
+++ Applicaties/Desktop/Appotheek application/Apotheek application/MasterPage.Designer.cs
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // MasterPage
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "MasterPage";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
