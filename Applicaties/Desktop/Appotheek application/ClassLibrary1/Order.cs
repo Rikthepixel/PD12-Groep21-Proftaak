0c1dbcca68e1b46bf3a4db3eecba1012701e1a0c
Aram Lemmens <460546@student.fontys.nl>
2020-11-30 10:21:51 +0100

    [Addition] Order class

---
+++ Applicaties/Desktop/Appotheek application/ClassLibrary1/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appotheekcl
+{
+    class Order
+    {
+        public ProductList Products { get; set; }
+    }
+
+    public void InsertNewOrder()
+    {
+
+    }
+}
