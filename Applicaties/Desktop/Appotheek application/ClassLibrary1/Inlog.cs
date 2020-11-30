894617fa9190ca5ed247357bdf4253f4a2957d58
Jimmy Schuurmans <jimmysch123@gmail.com>
2020-11-30 14:37:11 +0100

    [Add] Classes for Login

---
+++ Applicaties/Desktop/Appotheek application/ClassLibrary1/Inlog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appotheekcl
+{
+    class Inlog
+    {
+        public void loginUser()
+        {
+
+        }
+
+        public void generateUserLogin()
+        {
+
+        }
+        public void setLoginError()
+        {
+
+        }
+
+        public void setUserNametag()
+        {
+
+        }
+    }
+}
