9997cfec4d614a928fe84d450bcdcbacb44e030b
Aram Lemmens <460546@student.fontys.nl>
2020-11-30 14:36:17 +0100

    [Addition] OrderPagina Form

---
+++ Applicaties/Desktop/Appotheek application/Apotheek application/OrderPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apotheek_application
+{
+    public partial class OrderPage : Form
+    {
+        public OrderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
