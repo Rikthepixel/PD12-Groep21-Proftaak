6f5d6c18adb3dfebb3fb3b1e1216492cf9f557fd
RikThePixel <Rikdenbreejen@hotmail.nl>
2020-12-11 09:31:33 +0100

    [Addition] BOOTSTRAP STYLESHEET ADDED TO HEADER

--- Website/Header.inc.php
+++ Website/Header.inc.php
@@ -82,7 +82,7 @@
 </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="Styles/HeaderStyle.css" >
+    <link rel="stylesheet" href="Styles/HeaderStyle.css" >
 <style>
     #Help-btn{
 
@@ -92,7 +92,9 @@
             echo "padding: 4px 32.7px;";
         }?>
     }
+
 </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
 
