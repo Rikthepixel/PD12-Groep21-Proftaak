47bc7915048cc1d8b0436e5246fc4b2e7cc5e574
RIk <Rikdenbreejen@hotmail.nl>
2020-12-17 15:55:21 +0100

    [Addition] Added verification before adding a product

--- Website/Producten/BestellingUpdate.php
+++ Website/Producten/BestellingUpdate.php
@@ -1,25 +1,32 @@
 <?php
 include_once "../Include/DatabaseHandler.php";
-$Medicijn = $_POST["MDC"];
-$Toename = $_POST["TNM"];
-$GewichtQuery = "SELECT Gewicht FROM $Medicijn WHERE ID = '1'";
+include_once "../Include/SH.inc.php";
 
-$GewichtResult = $Prodsconn -> query($GewichtQuery);
-$GewichtArr = $GewichtResult -> fetch_array();
-$Gewicht = $GewichtArr[0];
-$CurrentDate = date('Y-m-d');
-$ExpiryMonth = date('m') + 2;
-$ExpiryYear = date('Y');
-if ($ExpiryMonth > 12)
-{
-    $ExpiryYear = $ExpiryYear + 1; 
-    $ExpiryMonth = $ExpiryMonth - 12;
+if(VerifySession()){
+    $Medicijn = $_POST["MDC"];
+    $Toename = $_POST["TNM"];
+    $GewichtQuery = "SELECT Gewicht FROM $Medicijn WHERE ID = '1'";
+    
+    $GewichtResult = $Prodsconn -> query($GewichtQuery);
+    $GewichtArr = $GewichtResult -> fetch_array();
+    $Gewicht = $GewichtArr[0];
+    $CurrentDate = date('Y-m-d');
+    $ExpiryMonth = date('m') + 2;
+    $ExpiryYear = date('Y');
+    if ($ExpiryMonth > 12)
+    {
+        $ExpiryYear = $ExpiryYear + 1; 
+        $ExpiryMonth = $ExpiryMonth - 12;
+    }
+    $ExpiryDate = date("$ExpiryYear-$ExpiryMonth-d");
+    
+    $Query = "INSERT INTO $Medicijn(Aantal, Gewicht, Datum_ontvangen, Uiterste_datum) 
+    VALUES('$Toename', '$Gewicht', '$CurrentDate', '$ExpiryDate')";
+    $Prodsconn -> query($Query);
+    
+    header("Location: ../../Bestellen.php");
+} else {
+    VerifySessionReturn("../../index.php", false)
 }
-$ExpiryDate = date("$ExpiryYear-$ExpiryMonth-d");
 
-$Query = "INSERT INTO $Medicijn(Aantal, Gewicht, Datum_ontvangen, Uiterste_datum) 
-VALUES('$Toename', '$Gewicht', '$CurrentDate', '$ExpiryDate')";
-$Prodsconn -> query($Query);
-
-header("Location: ../../Bestellen.php");
 ?>
