36fb9b764e00c5cc0ec96c240e072c341e62767b
RIk <Rikdenbreejen@hotmail.nl>
2021-01-05 10:04:42 +0100

    [Removed] the need for the database details
    We can just use the product connection, because why not

--- Website/Producten/FetchData.php
+++ Website/Producten/FetchData.php
@@ -1,5 +1,7 @@
 <?php
 include_once "../Include/SH.inc.php";
+include_once "../Include/DatabaseHandler.php";
+
 if(VerifySession()){
     $Query;
     $ConnectionStr;
@@ -7,29 +9,21 @@ if(VerifySession()){
     //Needs:
     //Cookies: Session & RequestingKey
     //To send query:
-    //DBServer, DBUser, DBPassword, DBName
     //SQLQuery
 
     if(isset($_COOKIE['RequestingKey'])){
-        if($_COOKIE['RequestingKey'] == $_SESSION['UserRequestKey'] && isset($_POST['SQLQuery'])){      
-            if(isset($_POST['DBServer']) && isset($_POST['DBUser']) && isset($_POST['DBPassword']) && isset($_POST['DBName'])){
-                $Query = $_POST['SQLQuery'];
-                $Conn = mysqli_connect($_POST['DBServer'], $_POST['DBUser'], $_POST['DBPassword'], $_POST['DBName']);
-                if (!$Conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
+        if($_COOKIE['RequestingKey'] == $_SESSION['UserRequestKey'] && isset($_POST['SQLQuery'])){
+            $Query = $_POST['SQLQuery'];
                 
-                $Result = $Conn -> query($Query);
+            $Result = $Prodsconn -> query($Query);
 
-                if ($Result->num_rows >= 0){
-                    $rows = array();
-                    while ($row = $Result -> fetch_assoc()){
-                      $rows[] = $row;
-                    }
-                    echo json_encode($rows);
+            if ($Result->num_rows >= 0){
+                $rows = array();
+                while ($row = $Result -> fetch_assoc()){
+                  $rows[] = $row;
                 }
+                echo json_encode($rows);
             }
-
         } else {
             VerifySessionReturn("../../index.php", false);
         }
