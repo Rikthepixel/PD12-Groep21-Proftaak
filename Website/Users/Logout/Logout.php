55584253d10e16fe89987b1b98be88e796651aa6
RIk <Rikdenbreejen@hotmail.nl>
2021-01-05 10:07:40 +0100

    [;] just a punt comma

--- Website/Users/Logout/Logout.php
+++ Website/Users/Logout/Logout.php
@@ -5,7 +5,7 @@ unset($_SESSION['Loggedin']);
 unset($_SESSION['Voornaam']);
 unset($_SESSION['Achternaam']);
 unset($_SESSION['Email']);
-unset($_SESSION['UserRequestKey'])
+unset($_SESSION['UserRequestKey']);
 
 unset($_COOKIE['RequestingKey']);
 setcookie('RequestingKey', '', time() - 3600, '/');
