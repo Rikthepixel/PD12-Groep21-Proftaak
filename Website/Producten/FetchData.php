6e50fb01f7b62bf35b062d3cd4cca8e6bcae4730
RIk <Rikdenbreejen@hotmail.nl>
2021-01-06 15:28:12 +0100

    [Change] Feedback if something fails

--- Website/Producten/FetchData.php
+++ Website/Producten/FetchData.php
@@ -5,18 +5,30 @@ include_once "../Include/SH.inc.php";
 if(VerifySession()){
     $Query;
 
+    if ($conn->connect_error){
+        die("Connection to the database fialed")
+    }
+
     if(isset($_COOKIE['RequestingKey'])){
         if($_COOKIE['RequestingKey'] == $_SESSION['UserRequestKey'] && isset($_POST['SQLQuery'])){
             $Query = $_POST['SQLQuery'];
                 
-            $Result = $Prodsconn -> query($Query);
-
-            if ($Result->num_rows >= 0){
-                $rows = array();
-                while ($row = $Result -> fetch_assoc()){
-                  $rows[] = $row;
+            if($Result = $Prodsconn -> query($Query) === true){
+                if ($Result->num_rows >= 0){
+                    $rows = array();
+                    while ($row = $Result -> fetch_assoc()){
+                      $rows[] = $row;
+                    }
+                    echo json_encode($rows);
                 }
-                echo json_encode($rows);
+                else{
+                    $row['succesful'] = "Query was succefully executed"
+                    echo json_encode()
+                }
+            }
+            else{
+                $row['succesful'] = "Unable to execute query"
+                echo json_encode()
             }
         } else {
             VerifySessionReturn("../../index.php", false);
