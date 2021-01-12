69284bf6b80dac6e7db26b703ec4db94d3f1534e
RIk <Rikdenbreejen@hotmail.nl>
2021-01-12 11:20:15 +0100

    [Added] Registration to api

--- Website/Producten/FetchData.php
+++ Website/Producten/FetchData.php
@@ -4,30 +4,12 @@ include_once "../Include/SH.inc.php";
 
 if(VerifySession()){
     if(isset($_COOKIE['RequestingKey'])){
-        if($_COOKIE['RequestingKey'] == $_SESSION['UserRequestKey'] && isset($_POST['SQLQuery'])){
-            $Query = $_POST['SQLQuery'];
-                
-            if($Result = $Prodsconn -> query($Query)){
-                if (is_object($Result)){
-                    if ($Result->num_rows >= 0){
-                        $returnData = array();
-                        while ($row = $Result -> fetch_assoc()){
-                          $returnData[] = $row;
-                        }
-                        echo json_encode($returnData);
-                    } else{
-                        $returnData['succesful'] = "Query was succefully executed";
-                        echo json_encode($returnData);
-                    }
-                }
-                else{
-                    $returnData['succesful'] = "Query was succefully executed";
-                    echo json_encode($returnData);
-                }
+        if($_COOKIE['RequestingKey'] == $_SESSION['UserRequestKey'] && isset($_POST['SQLQuery'])){          
+            if(isset($_POST['REGISTER'])){
+                ExecuteQuery($Loginconn, $_POST['SQLQuery']);
             }
             else{
-                $returnData['succesful'] = "Unable to execute query";
-                echo json_encode($returnData);
+                ExecuteQuery($Prodsconn, $_POST['SQLQuery']);
             }
         } else {
             ReturnToLogin();
@@ -41,5 +23,30 @@ if(VerifySession()){
 
 function ReturnToLogin(){
     VerifySessionReturn("../../index.php", false);
+}
+
+function ExecuteQuery($Connection, $Query){
+    if($Result = $Connection -> query($Query)){
+        if (is_object($Result)){
+            if ($Result->num_rows >= 0){
+                $returnData = array();
+                while ($row = $Result -> fetch_assoc()){
+                  $returnData[] = $row;
+                }
+                echo json_encode($returnData);
+            } else{
+                $returnData['succesful'] = "Query was succefully executed";
+                echo json_encode($returnData);
+            }
+        }
+        else{
+            $returnData['succesful'] = "Query was succefully executed";
+            echo json_encode($returnData);
+        }
+    }
+    else{
+        $returnData['succesful'] = "Unable to execute query";
+        echo json_encode($returnData);
+    }
 }
 ?>
